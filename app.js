254fa4d1004eb09be2c9806785f203640db56f12
Kashyap Murthy <kashyap.skash@gmail.com>
2017-07-11 20:53:42 +0530

    Initial code to checkout react-native-socket-io-example

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app)
+var port = process.env.PORT || 3000;
+var io = require("socket.io")(http);
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket){
+    console.log("a user is connected")
+    socket.on("disconnect", function(){
+        console.log("user disconnected");
+    });
+    socket.on("chat message", function(msg){
+        console.log("message: " + msg);
+    });
+})
+
+http.listen(port, function() {
+    console.log("listening on 3000");
+});
