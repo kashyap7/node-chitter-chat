396887092ec9d3d224728c0c87cc92fea777be80
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-08-06 01:21:14 +0530

    Add body-parser and api routes

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const config = require("config");
+const bodyParser = require("body-parser");
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 // db options
 let options = { 
   server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
@@ -13,12 +16,15 @@ let options = {
   };
 
 //db connection
+console.log(config.DBHost)
 mongoose.connect(config.DBHost, options)
   .then(function(){
     console.log("Mongo connection successful");
   }).catch(function(error){
     console.log(error.message)
   });
+
+require(__dirname + "/controllers/userController.js")(app);
 
 app.get("/", (req, res) => {
   res.json("Welcome to Chitter messaging app");
