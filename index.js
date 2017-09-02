606fc203787392e19d42dc9fa257909db4af5b56
Kashyap Murthy <kashyap.skash@gmail.com>
2017-09-03 00:36:13 +0530

    Implement the tasks suggested in the socket.io tutorial

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  // We fetch the username when the user connect.
+  var userName;
+  socket.on('on connect', (name) => {
+  	userName = name;
+  	console.log(name);
   });
+  socket.on('chat message', function(msg){
+    // we broadcast the message the user wants to send to other user
+    socket.broadcast.emit('chat message', msg);
+  });
+  // we broadcast to all other users still connected once the user disconnects.
+  socket.on('disconnect', function(){
+    io.emit('chat message', userName + ': user disconnected');
+  });	
 });
 
+// The port the user connects to.
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
